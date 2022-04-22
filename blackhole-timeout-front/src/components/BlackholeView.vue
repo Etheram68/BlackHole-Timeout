34b61f9aad52a0a1c079fd9af47bd20913d8d548
Etheram68 <francois681927@hotmail.fr>
2022-04-22 14:26:42 +0200

    Update code for removed infinity loop

--- blackhole-timeout-front/src/components/BlackholeView.vue
+++ blackhole-timeout-front/src/components/BlackholeView.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>
+	<div class="users-bh">
 		<ul class="grid h-full place-items-center justify-cente gap-x-8 gap-y-4 lg:grid-cols-4 p-8">
 			<li class="max-w-md bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl" v-for="(user, index) in users" v-bind:key="index">
 				<item-user :user=user :convertDate=convertDate></item-user>
@@ -17,37 +17,38 @@
 		data() {
 			return {
 				users: [],
-				new_date: '',
 				page_index: 1
 			}
 		},
 		methods: {
 			convertDate: function(str_date) {
-				this.new_date = new Date(str_date).toUTCString().split(' ').slice(0, 4).join(' ')
-
-				return this.new_date
+				return new Date(str_date).toUTCString().split(' ').slice(0, 4).join(' ')
 			},
-			logger: function(trace){
+			logger: function(trace) {
 				console.log(trace)
+			},
+			getUsers: function() {
+				axios
+				.get('http://localhost:5280/users/blackhole-timeout', {
+					headers: {
+						"Access-Control-Allow-Origin": "*"
+					},
+					params: {
+						page: this.page_index
+					}
+				})
+				.then(response => {
+					console.log(response.data.data);
+					this.users.push(...response.data.data);
+					this.page_index++;
+				})
 			}
 		},
 		components: {
 			'item-user': ItemUser
 		},
 		mounted() {
-			axios
-			.get('http://localhost:5280/users/blackhole-timeout', {
-				headers: {
-					"Access-Control-Allow-Origin": "*"
-				},
-				params: {
-					page: 1
-				}
-			})
-			.then(response => {
-				console.log(response.data.data)
-				this.users = response.data.data;
-			})
+			this.getUsers()
 		}
 	}
 </script>
