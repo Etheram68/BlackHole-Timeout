b29a5c65040bf9ab00cc8d4d5c26fa0316ca0b9b
Etheram68 <francois681927@hotmail.fr>
2022-04-26 12:06:07 +0200

    replace title by title beautiful

--- blackhole-timeout-front/src/components/ItemUser.vue
+++ blackhole-timeout-front/src/components/ItemUser.vue
@@ -4,10 +4,10 @@
 			<img class="h-48 w-full object-cover sm:h-full sm:w-48" :src="urlImg=user.image_url" :alt="'profile_'+user.login">
 		</div>
 		<div class="p-8">
-			<div id="login" class="uppercase tracking-wide text-sm text-indigo-500 font-semibold" @click.prevent="openProfile(user.login)" :title="title=concatUrl(user.login)">{{ user.login }}</div>
+			<div id="login" class="uppercase tracking-wide text-sm text-indigo-500 font-semibold" @click.prevent="openProfile(user.login)" :data_title="data_title=concatUrl(user.login)">{{ user.login }}</div>
 			<p class="mt-2 text-slate-500">Email: {{ user.email }}</p>
 			<p class="mt-2 text-slate-500">Days Left: {{ user.days_left }}</p>
-			<p class="mt-2 text-slate-500" :title="title=convertDate(user.last_connection)">Last connexion: {{ deltaDays(user.last_connection) }} days</p>
+			<p class="mt-2 text-slate-500" :data_title="data_title=convertDate(user.last_connection)">Last connexion: {{ deltaDays(user.last_connection) }} days</p>
 		</div>
 	</div>
 </template>
@@ -31,5 +31,35 @@
 <style scoped>
 	#login {
 		cursor: pointer;
+	}
+
+	[data_title]:hover:after {
+		opacity: 1;
+		transition: all 0.2s ease 0.6s;
+		visibility: visible;
+	}
+	[data_title]:after {
+		content: attr(data_title);
+		font-size: 0.6em;
+		position: absolute;
+		display: flex;
+		padding: 4px 8px 4px 8px;
+		color: #222;
+		background-color: #fff;
+		border-radius: 5px;
+		box-shadow: 0px 0px 15px #222;
+		background-image: -webkit-linear-gradient(
+							top, #f8f8f8, #cccccc);
+
+		background-image: -moz-linear-gradient(
+							top, #f8f8f8, #cccccc);
+
+		background-image: -ms-linear-gradient(
+							top, #f8f8f8, #cccccc);
+
+		background-image: -o-linear-gradient(
+							top, #f8f8f8, #cccccc);
+
+		visibility: hidden;
 	}
 </style>
