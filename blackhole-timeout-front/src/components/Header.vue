70f68c7f14951daeddb1ee088a4ec1f5a9fbfb8b
Etheram68 <francois681927@hotmail.fr>
2022-04-26 11:23:41 +0200

    Update header and add info to ItemUser Title

--- blackhole-timeout-front/src/components/Header.vue
+++ blackhole-timeout-front/src/components/Header.vue
@@ -1,23 +1,33 @@
 <template>
-	<header class="bg-gray-900 sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3">
-		<div class="flex items-center justify-between px-4 py-3 sm:p-0">
-			<div>
-				<!-- <img class="h-8" src="/img/logo-inverted.svg" alt="Workcation"> -->
-				<router-link class="h-8 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" to="/">42 Blackhole timeout</router-link>
-			</div>
-			<div class="sm:hidden">
-				<button @click="isOpen = !isOpen" type="button" class="block text-gray-500 hover:text-white focus:text-white focus:outline-none">
+	<header class="sticky top-0 z-50">
+		<nav class="relative flex flex-wrap items-center justify-between px-2 py-3 bg-cyan-500 mb-3">
+			<div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
+				<div class="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
+				<router-link class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" to="/">
+					42 Blackhole Timeout
+				</router-link>
+				<button class="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button" v-on:click="toggleNavbar()">
 					<svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
 						<path v-if="isOpen" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
 						<path v-if="!isOpen" fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
 					</svg>
 				</button>
+				</div>
+				<div v-bind:class="{'hidden': !showMenu, 'flex': showMenu}" class="lg:flex lg:flex-grow items-center">
+				<ul class="flex flex-col lg:flex-row list-none ml-auto">
+					<li class="nav-item">
+						<router-link class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" to="/">
+							<span class="ml-2">Home</span>
+						</router-link>
+					</li>
+					<li class="nav-item">
+						<router-link class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" to="/blackhole">
+							<span class="ml-2">Blackhole</span>
+						</router-link>
+					</li>
+				</ul>
+				</div>
 			</div>
-		</div>
-		<nav :class="isOpen ? 'block' : 'hidden'" class="px-2 pt-2 pb-4 sm:flex sm:p-0">
-			<router-link to="/" class="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800">Home</router-link>
-			<router-link to="/blackhole" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2">Blackhole</router-link>
-			<!-- <a href="#" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2">Messages</a> -->
 		</nav>
 	</header>
 </template>
@@ -27,7 +37,13 @@
 		name: 'HeaderTop',
 		data() {
 			return {
+				showMenu: false,
 				isOpen: false,
+			}
+		},
+		methods: {
+			toggleNavbar: function(){
+				this.showMenu = !this.showMenu;
 			}
 		}
 	}
--- blackhole-timeout-front/src/components/ItemUser.vue
+++ blackhole-timeout-front/src/components/ItemUser.vue
@@ -4,7 +4,7 @@
 			<img class="h-48 w-full object-cover sm:h-full sm:w-48" :src="urlImg=user.image_url" :alt="'profile_'+user.login">
 		</div>
 		<div class="p-8">
-			<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold" @click.prevent="openProfile(user.login)">{{ user.login }}</div>
+			<div id="login" class="uppercase tracking-wide text-sm text-indigo-500 font-semibold" @click.prevent="openProfile(user.login)" :title="title=concatUrl(user.login)">{{ user.login }}</div>
 			<p class="mt-2 text-slate-500">Email: {{ user.email }}</p>
 			<p class="mt-2 text-slate-500">Days Left: {{ user.days_left }}</p>
 			<p class="mt-2 text-slate-500" :title="title=convertDate(user.last_connection)">Last connexion: {{ deltaDays(user.last_connection) }} days</p>
@@ -15,9 +15,21 @@
 <script>
 
 	export default {
+		name: 'ItemUser',
 		props: ['user', 'convertDate', 'deltaDays'],
+		methods: {
+			openProfile: function(login) {
+				window.open('https://profile.intra.42.fr/users/'+login, '_blank');
+			},
+			concatUrl: function(login) {
+				return 'https://profile.intra.42.fr/users/'+login
+			}
+		}
 	}
 </script>
 
 <style scoped>
+	#login {
+		cursor: pointer;
+	}
 </style>
